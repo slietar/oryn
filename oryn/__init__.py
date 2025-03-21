6c3cd209e0855607890a6ab965f05cb97be0c1bb
Simon Liétar <simon@lietar.net>
2025-03-21 15:16:53 +0100

    Improve metadata transfer to wheel

--- oryn/__init__.py
+++ oryn/__init__.py
@@ -5,8 +5,7 @@ import tomllib
 from io import StringIO
 from pathlib import Path
 from pprint import pprint
-from zipfile import Path as ZipInfo
-from zipfile import ZipFile
+from zipfile import ZipFile, ZipInfo
 
 from packaging.version import InvalidVersion, Version
 
@@ -72,11 +71,13 @@ def build_wheel(wheel_directory: str, config_settings = None, metadata_directory
     raise ValueError(f'Invalid version: {raw_version}')
 
 
-  # Find targets
+  # Load metadata
 
   root_path = Path.cwd()
 
-  _, tool_metadata = read_metadata(root_path)
+  metadata, tool_metadata = read_metadata(root_path)
+  project_metadata = metadata.get('project', {})
+
   ignore_rules = get_ignore_rules(root_path, tool_metadata)
 
 
@@ -85,16 +86,12 @@ def build_wheel(wheel_directory: str, config_settings = None, metadata_directory
   wheel_file_name = f'{name}-{version}-py3-none-any.whl'
 
   with ZipFile(Path(wheel_directory) / wheel_file_name, 'w') as archive:
+    # Initialize
+
     # See: https://packaging.python.org/en/latest/specifications/recording-installed-packages/
     dist_info_path = Path(f'{name}-{version}.dist-info')
 
     # with ZipPath(archive, str(dist_info_path / 'WHEEL')).open('w') as wheel_file:
-    with archive.open(str(dist_info_path / 'WHEEL'), 'w') as wheel_file:
-      wheel_file.write(f'Wheel-Version: 1.0\n'.encode())
-
-    with archive.open(str(dist_info_path / 'METADATA'), 'w') as metadata_file:
-      metadata_file.write(f'Metadata-Version: 1.0\nName: {name}\nVersion: {version}\n'.encode())
-
     record_output = StringIO()
     record_writer = csv.writer(record_output, delimiter=',', lineterminator='\n')
 
@@ -114,13 +111,62 @@ def build_wheel(wheel_directory: str, config_settings = None, metadata_directory
       source_path = root_path / target_path
 
       # Using this instead of archive.write() to erase timestamp
-      with source_path.open('rb') as source_file, archive.open(target_info, 'w') as target_file:
+      with (
+        source_path.open('rb') as source_file,
+        archive.open(target_info, 'w') as target_file
+      ):
         shutil.copyfileobj(source_file, target_file, 1024 * 8)
 
       record_writer.writerow([target_path_str, '', ''])
 
+    # Write metadata, record and wheel files
+
+    with archive.open(str(dist_info_path / 'METADATA'), 'w') as metadata_file:
+      metadata_file.write(f'Metadata-Version: 1.0\nName: {name}\nVersion: {version}\n'.encode())
+
+      if 'requires-python' in project_metadata:
+        metadata_file.write(f'Requires-Python: {project_metadata['requires-python']}\n'.encode())
+
+      if 'description' in project_metadata:
+        metadata_file.write(f'Summary: {project_metadata['description']}\n'.encode())
+
+      if 'dependencies' in project_metadata:
+        for dependency in project_metadata['dependencies']:
+          metadata_file.write(f'Requires-Dist: {dependency}\n'.encode())
+
+      if 'readme' in project_metadata:
+        if isinstance(project_metadata['readme'], str):
+          readme_path = Path(project_metadata['readme'])
+          readme_type = None
+        else:
+          readme_path = Path(project_metadata['readme']['file'])
+          readme_type = project_metadata['readme'].get('content-type')
+
+        if not readme_path.is_absolute():
+          readme_path = root_path / readme_path
+
+        if readme_type is None:
+          match readme_path.suffix.lower():
+            case '.md':
+              readme_type = 'text/markdown'
+            case '.rst':
+              readme_type = 'text/x-rst'
+            case _:
+              readme_type = 'text/plain'
+
+        metadata_file.write(f'Description-Content-Type: {readme_type}\n\n'.encode())
+
+        with readme_path.open('rb') as readme_file:
+          shutil.copyfileobj(readme_file, metadata_file)
+
+    with archive.open(str(dist_info_path / 'METADATA'), 'r') as metadata_file:
+      print(metadata_file.read().decode())
+
     with archive.open(str(dist_info_path / 'RECORD'), 'w') as record_file:
       record_file.write(record_output.getvalue().encode())
+
+    with archive.open(str(dist_info_path / 'WHEEL'), 'w') as wheel_file:
+      wheel_file.write(f'Wheel-Version: 1.0\n'.encode())
 
 
     print('-- Wheel contents --------')
